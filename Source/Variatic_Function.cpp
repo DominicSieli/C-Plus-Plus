9a58d4bc053f5e60fbda8d43030e97fa4a926647
dominisieli <dominicsieli@gmail.com>
2020-12-06 17:44:41 -0700

    Added Variatic_Function.cpp and Variatic_Template.cpp

---
+++ Source/Variatic_Function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void Variatic_Function(const unsigned int& n, ...)
+{
+	va_list list;
+	va_start(list, n);
+	
+	for(unsigned int i = 0; i < n; i++)
+	{
+	    std::cout << va_arg(list, int) << " ";
+	}
+	
+	va_end(list);
+	
+	std::cout << '\n';
+}
+
+int main()
+{
+	Variatic_Function(6,1,2,3,4,5,6);
+}
