b5ae9270c95fe80c5d49bb7cae3e27accffc8aca
dominisieli <dominicsieli@gmail.com>
2020-12-09 21:00:56 -0700

    Updated Move_Semantics.cpp

--- Source/File_IO.cpp
+++ Source/File_IO.cpp
@@ -13,11 +13,11 @@ int main()
 
 	readFile.open("File_IO" SLASH "Output.txt", std::ios_base::in);
 
-	if (readFile.is_open())
+	if(readFile.is_open())
 	{
 		std::string text = "";
 
-		while (readFile.good())
+		while(readFile.good())
 		{
 			getline(readFile, text);
 
@@ -33,11 +33,11 @@ int main()
 
 	writeFile.open("File_IO" SLASH "Output.txt", std::ios_base::out | std::ios_base::trunc);
 
-	if (writeFile.is_open())
+	if(writeFile.is_open())
 	{
 		std::string text = "";
 
-		for (int i = 1; i < 21; i++)
+		for(int i = 1; i < 21; i++)
 		{
 			text += "Line: " + std::to_string(i) + '\n';
 		}
--- Source/Move_Semantics.cpp
+++ Source/Move_Semantics.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
 
+template<typename T>
+class Array
+{
+private:
+	unsigned int n = 0;
+	T* array = nullptr;
+
+public:
+	Array(const unsigned int& n) : n{n}, array{new T[n]}
+	{
+
+	}
+
+	Array(Array&& move_class) : n{move_class.n}, array{move_class.array}
+	{
+		std::cout << "Array Moved" << '\n';
+	}
+
+	Array(const Array& copy_class) : n{copy_class.n}, array{copy_class.array}
+	{
+		std::cout << "Array Copied" << '\n';
+	}
+
+	Array& operator=(Array&& move_class)
+	{
+		n = {move_class.n};
+		array = {move_class.array};
+		std::cout << "Array Moved" << '\n';
+		return *this;
+	}
+
+	Array& operator=(const Array& copy_class)
+	{
+		n = {copy_class.n};
+		array = {copy_class.array};
+		std::cout << "Array Copied" << '\n';
+		return *this;
+	}
+
+	T& operator[](const unsigned int& index)
+	{
+		return array[index];
+	}
+
+	unsigned int Size() const
+	{
+		return n;
+	}
+};
+
 int main()
 {
-	std::cout << "Test" << '\n';
+	Array<int> array_1(10);
+
+	for(unsigned int i = 0; i < array_1.Size(); i++)
+	{
+		array_1[i] = i;
+	}
+
+	Array<int> array_2(std::move(array_1));
+	//array_2 = std::move(array_1);
+
+	for(unsigned int i = 0; i < array_2.Size(); i++)
+	{
+		std::cout << array_2[i] << '\n';
+	}
 
 	std::cin.get();
 }
